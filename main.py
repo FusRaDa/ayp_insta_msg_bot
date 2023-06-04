0adeacff34e2628b4359a4c32467bb0c89ae6166
Matthew Rada <matthewr7.christopher@gmail.com>
2023-06-04 14:20:05 -0400

    login successful

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import selenium.common.exceptions
 import configparser
+import time
 
 
 # https://www.geeksforgeeks.org/send-direct-message-on-instagram-using-selenium-in-python/
@@ -8,15 +11,39 @@ import configparser
 parser = configparser.ConfigParser()
 parser.read('credentials.txt')
 
+username = parser.get('creds', 'username')
+password = parser.get('creds', 'password')
+
+# go to instagram
+chrome = webdriver.Chrome()
+chrome.get('https://www.instagram.com/')
+
+
+def login():
+
+    time.sleep(5)
+
+    # finds the username box
+    username_input = chrome.find_element("name", "username")
+    # sends the entered username
+    username_input.send_keys(username)
+
+    # finds the password box
+    password_input = chrome.find_element("name", "password")
+    # sends the entered password
+    password_input.send_keys(password)
+
+    # press enter after sending password
+    password_input.send_keys(Keys.RETURN)
+    time.sleep(5)
 
 
 
-def path():
-    chrome = webdriver.Chrome()
-    chrome.get('https://www.instagram.com/')
 
 
-path()
+
+
+
 
 
 
